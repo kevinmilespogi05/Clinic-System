97c2f679871b8868f866d3afe9ebb9ecb296cdb8
sekanflor <aschilledeleon06@gmail.com>
2024-12-08 17:07:09 +0800

    dashboard

--- patient-management/src/app/user/dashboard/dashboard.component.ts
+++ patient-management/src/app/user/dashboard/dashboard.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  templateUrl: './dashboard.component.html', // Ensure this path is correct
+  styleUrls: ['./dashboard.component.css']   // Ensure this path is correct
 })
 export class DashboardComponent {
-
+  // Component logic
 }
