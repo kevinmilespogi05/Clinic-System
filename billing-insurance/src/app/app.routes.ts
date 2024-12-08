4c98cd68a19aacb9b8072ab07c32470b3790e26d
kevinmilespogi05 <kevinmilesjulhusin05@gmail.com>
2024-12-08 15:51:48 +0800

    Update app.routes.ts

--- billing-insurance/src/app/app.routes.ts
+++ billing-insurance/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './user/login/login.component';
+import { InsuranceComponent } from './user/insurance/insurance.component';
+import { InvoiceComponent } from './user/invoice/invoice.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'login'},
     { path: 'login', component: LoginComponent, title: 'Login' },
+    { path: 'insurance', component: InsuranceComponent, title: 'Insurance' },
+    { path: 'invoice', component: InvoiceComponent, title: 'Invoice' },
 ];
