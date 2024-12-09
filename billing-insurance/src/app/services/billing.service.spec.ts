59f9581ac45a3e2057b5aee41751d8112c2ee007
acgbm <157551924+acgbm@users.noreply.github.com>
2024-12-09 17:05:42 +0800

    billing services

--- billing-insurance/src/app/services/billing.service.spec.ts
+++ billing-insurance/src/app/services/billing.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiService } from './api.service';
+import { BillingService } from './billing.service';
 
-describe('ApiService', () => {
-  let service: ApiService;
+describe('BillingService', () => {
+  let service: BillingService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
+    service = TestBed.inject(BillingService);
   });
 
   it('should be created', () => {
