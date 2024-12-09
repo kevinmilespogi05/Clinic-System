aa4986343bcd9dce847bde078edd03ab3ee80e58
acgbm <157551924+acgbm@users.noreply.github.com>
2024-12-10 01:14:09 +0800

    DATABASE UPDATE!!!!

--- clinicapi/clinic_billing.sql
+++ clinicapi/clinic_billing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2024 at 04:24 AM
+-- Generation Time: Dec 09, 2024 at 06:13 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,21 +28,29 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `appointments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `appointment_date` date NOT NULL,
-  `appointment_time` time NOT NULL,
-  `status` enum('booked','cancelled') DEFAULT 'booked',
-  PRIMARY KEY (`id`)
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 
 --
 -- Dumping data for table `appointments`
 --
 
-
-
+INSERT INTO `appointments` (`id`, `user_id`, `date`, `time`, `description`, `created_at`) VALUES
+(1, 5, '2024-12-10', '10:00:00', 'General checkup', '2024-12-09 15:00:27'),
+(2, 5, '2024-12-10', '10:00:00', 'General checkup', '2024-12-09 15:10:17'),
+(4, 10, '2024-12-25', '10:00:00', NULL, '2024-12-09 15:54:02'),
+(5, 10, '2024-02-11', '08:00:00', NULL, '2024-12-09 15:58:50'),
+(6, 10, '2024-11-22', '07:45:00', NULL, '2024-12-09 16:12:37'),
+(7, 10, '2025-11-22', '19:00:00', NULL, '2024-12-09 16:16:01'),
+(8, 10, '2323-02-11', '12:32:00', NULL, '2024-12-09 16:29:47'),
+(9, 10, '2322-02-23', '02:32:00', NULL, '2024-12-09 16:33:45'),
+(10, 10, '0000-00-00', '02:32:00', 'adhd', '2024-12-09 16:37:24'),
+(11, 10, '2122-12-31', '14:32:00', 'I need eye checkup', '2024-12-09 16:37:51');
 
 -- --------------------------------------------------------
 
@@ -117,7 +125,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `medical_records`, `created_a
 (6, 'ladyjohan', '$2y$10$Ln8DvG4AcusY6bpwRzUWg.fClKJF57eU4GmJo2W8UceGaNViHfMCS', NULL, '2024-12-08 14:05:48', 'Lady Johan Canapate', '09389077792', '2004-09-30', ''),
 (7, 'hex', '$2y$10$83BXTgejWJnxFOtlwrRe3OxJUxly6jdNLXOvrEvQxIE8KqeT6OPIi', NULL, '2024-12-08 14:30:14', 'HexsuDane', '09389077792', '2004-09-30', ''),
 (8, 'kevinhindipogi', '$2y$10$T/BtQV9apWwP2F4PH1lBheviREiOB/FT8wNzwkpm0/JuJMu59m/kW', NULL, '2024-12-09 03:09:28', 'kevin', '0912312312', '2003-02-23', ''),
-(9, 'johnkevin', '$2y$10$c5yrcd2TNbZ0fr9g0sFHTuwu3x2pmfPhHFUBeLqVU7h0unOgRpg5u', NULL, '2024-12-09 03:23:17', 'kevin', '1234567890', '1990-01-01', '');
+(9, 'johnkevin', '$2y$10$c5yrcd2TNbZ0fr9g0sFHTuwu3x2pmfPhHFUBeLqVU7h0unOgRpg5u', NULL, '2024-12-09 03:23:17', 'kevin', '1234567890', '1990-01-01', ''),
+(10, 'LJ', '$2y$10$zH0rBop6n/ALb46klQ2BQeYkRDoXRueCncbPNvS5COg5Z5D.xmp4O', NULL, '2024-12-09 09:27:14', 'Lady Johan Canapate', '09389077792', '2004-09-30', '');
 
 --
 -- Indexes for dumped tables
@@ -127,8 +136,7 @@ INSERT INTO `users` (`id`, `username`, `password`, `medical_records`, `created_a
 -- Indexes for table `appointments`
 --
 ALTER TABLE `appointments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `insurance_claims`
@@ -159,7 +167,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `insurance_claims`
@@ -177,17 +185,11 @@ ALTER TABLE `invoices`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `appointments`
---
-ALTER TABLE `appointments`
-  ADD CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `insurance_claims`
