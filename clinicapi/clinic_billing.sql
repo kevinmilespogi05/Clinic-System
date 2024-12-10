9dc8a530ba39955ed5bbc53996271d2f8059cece
acgbm <157551924+acgbm@users.noreply.github.com>
2024-12-10 19:57:04 +0800

    DB UPDATE AGAIN!!!

--- clinicapi/clinic_billing.sql
+++ clinicapi/clinic_billing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2024 at 06:11 AM
+-- Generation Time: Dec 10, 2024 at 12:55 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -44,7 +44,8 @@ CREATE TABLE `appointments` (
 INSERT INTO `appointments` (`id`, `user_id`, `name`, `date`, `time`, `description`, `created_at`) VALUES
 (12, 9, 'kevin', '2024-12-15', '10:30:00', 'Dental Checkup', '2024-12-10 04:43:40'),
 (13, 5, 'ac', '2024-03-04', '14:03:00', 'HAHAHA', '2024-12-10 04:52:10'),
-(14, 4, 'John Doe', '2024-02-23', '02:32:00', 'adhd', '2024-12-10 05:09:23');
+(14, 4, 'John Doe', '2024-02-23', '02:32:00', 'adhd', '2024-12-10 05:09:23'),
+(15, 11, 'Hex Cee', '2024-12-25', '17:30:00', 'checkup', '2024-12-10 06:07:56');
 
 -- --------------------------------------------------------
 
@@ -103,21 +104,18 @@ CREATE TABLE `users` (
   `name` varchar(255) NOT NULL,
   `contact_number` varchar(20) NOT NULL,
   `date_of_birth` date DEFAULT NULL,
-  `medical_history` text NOT NULL
+  `medical_history` text NOT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `medical_records`, `created_at`, `name`, `contact_number`, `date_of_birth`, `medical_history`) VALUES
-(1, 'admin', '$2y$10$V9trfojqYAFNUBT.lMkf5eCUMzowF6R4P7tNvc29zh6Pm3IfFizVS', NULL, '2024-12-08 07:04:59', 'John Doe', '1234567890', '1990-01-01', ''),
-(2, 'john_doe', '$2y$10$N.DGZx3EHgTSBg5EWeIDheYrbfTjHVirlwWeiEUX.lg75B8EN0v3W', 'Allergic to penicillin.', '2024-12-08 07:29:13', '', '', NULL, ''),
-(3, 'acgabriel', '$2y$10$Da3TEt/jmHQ8fHN5yh1eQefRsv27QuffZLFV..PooYctNm0sNaWk2', NULL, '2024-12-08 11:07:24', 'Ac Gabriel', '', NULL, 'No known allergies. Previous surgery in 2015.'),
-(4, 'johndoe', '$2y$10$3bso9jc3i3Hw5ZgAZYwXgeYrmmlt0THcZoa.nr2rrWugeXpXA2nHq', NULL, '2024-12-08 12:08:31', 'John Doe', '1234567890', '1990-01-01', 'No known conditions'),
-(5, 'ac22', '$2y$10$.af5wH7IPoP5yiWrnmnrp.KNXmxVmmgnZKiVv7G60H2qUUijIXK4q', NULL, '2024-12-08 13:58:41', 'ac', '09926591335', '2003-11-22', ''),
-(8, 'kevinhindipogi', '$2y$10$T/BtQV9apWwP2F4PH1lBheviREiOB/FT8wNzwkpm0/JuJMu59m/kW', NULL, '2024-12-09 03:09:28', 'kevin', '0912312312', '2003-02-23', ''),
-(9, 'johnkevin', '$2y$10$c5yrcd2TNbZ0fr9g0sFHTuwu3x2pmfPhHFUBeLqVU7h0unOgRpg5u', NULL, '2024-12-09 03:23:17', 'kevin', '1234567890', '1990-01-01', '');
+INSERT INTO `users` (`id`, `username`, `password`, `medical_records`, `created_at`, `name`, `contact_number`, `date_of_birth`, `medical_history`, `role`) VALUES
+(1, 'admin', '$2y$10$uSa9VcjLIhfwpkuA2waNL.KCgBa8lRH8inUVYqOEOy4jywV9bm4ai', NULL, '2024-12-08 07:04:59', 'ADMINISTRATOR', '09926591335', '2003-11-22', '', 'admin'),
+(12, 'acgbm', '$2y$10$I2W4UVETPUtd8Fy0lgzRIOWvaTbpBebABKcoRi2IEom9donFM9oAC', NULL, '2024-12-10 11:30:13', 'ac manalo', '09150540618', '2003-11-22', '', 'user'),
+(13, 'acc', '$2y$10$dNV8dU.CwQH36TR/j2MTEuiTuwopPS4M.ZmWYK21jHVa8RYMs5d/K', NULL, '2024-12-10 11:34:03', 'ac', '123', '2323-03-22', '', 'user');
 
 --
 -- Indexes for dumped tables
@@ -158,7 +156,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `insurance_claims`
@@ -176,7 +174,7 @@ ALTER TABLE `invoices`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
