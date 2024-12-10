b862917635215c67ec24e9726edc3f29afa2b231
acgbm <157551924+acgbm@users.noreply.github.com>
2024-12-11 04:29:01 +0800

    UPDATED DATABASE AGAIN!!!

--- clinicapi/clinic_billing.sql
+++ clinicapi/clinic_billing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2024 at 12:55 PM
+-- Generation Time: Dec 10, 2024 at 09:27 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -34,18 +34,26 @@ CREATE TABLE `appointments` (
   `date` date NOT NULL,
   `time` time NOT NULL,
   `description` text DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` enum('booked','cancelled') NOT NULL DEFAULT 'booked'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `appointments`
 --
 
-INSERT INTO `appointments` (`id`, `user_id`, `name`, `date`, `time`, `description`, `created_at`) VALUES
-(12, 9, 'kevin', '2024-12-15', '10:30:00', 'Dental Checkup', '2024-12-10 04:43:40'),
-(13, 5, 'ac', '2024-03-04', '14:03:00', 'HAHAHA', '2024-12-10 04:52:10'),
-(14, 4, 'John Doe', '2024-02-23', '02:32:00', 'adhd', '2024-12-10 05:09:23'),
-(15, 11, 'Hex Cee', '2024-12-25', '17:30:00', 'checkup', '2024-12-10 06:07:56');
+INSERT INTO `appointments` (`id`, `user_id`, `name`, `date`, `time`, `description`, `created_at`, `status`) VALUES
+(12, 9, 'kevin', '2024-12-15', '10:30:00', 'Dental Checkup', '2024-12-10 04:43:40', 'booked'),
+(13, 5, 'ac', '2024-03-04', '14:03:00', 'HAHAHA', '2024-12-10 04:52:10', 'booked'),
+(14, 4, 'John Doe', '2024-02-23', '02:32:00', 'adhd', '2024-12-10 05:09:23', 'booked'),
+(15, 11, 'Hex Cee', '2024-12-25', '17:30:00', 'checkup', '2024-12-10 06:07:56', 'booked'),
+(16, 15, 'Ac Gabriel Manalo', '2004-02-21', '15:43:00', 'fasdfasd', '2024-12-10 17:13:30', 'booked'),
+(17, 15, 'Ac Gabriel Manalo', '2034-04-23', '15:12:00', 'asdfasafgg', '2024-12-10 17:13:40', 'cancelled'),
+(18, 15, 'Ac Gabriel Manalo', '2024-03-04', '16:32:00', '45353456', '2024-12-10 17:13:52', 'booked'),
+(19, 15, 'Ac Gabriel Manalo', '2034-03-31', '15:24:00', 'ncxbnc', '2024-12-10 17:14:04', 'cancelled'),
+(20, 15, 'Ac Gabriel Manalo', '4322-03-04', '16:23:00', 'ncvmfghjf', '2024-12-10 17:14:11', 'cancelled'),
+(21, 15, 'Ac Gabriel Manalo', '3222-03-22', '14:32:00', 'fasfadfgdsvz', '2024-12-10 17:14:39', 'cancelled'),
+(22, 15, 'Ac Gabriel Manalo', '0003-02-21', '02:13:00', 'asdfasdfaerfa', '2024-12-10 20:26:26', 'booked');
 
 -- --------------------------------------------------------
 
@@ -114,8 +122,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `medical_records`, `created_at`, `name`, `contact_number`, `date_of_birth`, `medical_history`, `role`) VALUES
 (1, 'admin', '$2y$10$uSa9VcjLIhfwpkuA2waNL.KCgBa8lRH8inUVYqOEOy4jywV9bm4ai', NULL, '2024-12-08 07:04:59', 'ADMINISTRATOR', '09926591335', '2003-11-22', '', 'admin'),
-(12, 'acgbm', '$2y$10$I2W4UVETPUtd8Fy0lgzRIOWvaTbpBebABKcoRi2IEom9donFM9oAC', NULL, '2024-12-10 11:30:13', 'ac manalo', '09150540618', '2003-11-22', '', 'user'),
-(13, 'acc', '$2y$10$dNV8dU.CwQH36TR/j2MTEuiTuwopPS4M.ZmWYK21jHVa8RYMs5d/K', NULL, '2024-12-10 11:34:03', 'ac', '123', '2323-03-22', '', 'user');
+(15, 'acgbm', '$2y$10$9FVsrYDLUM7dti3yAY.52OWmGITNM4pSGQPhOV0zHeJrtIbpXJs/2', NULL, '2024-12-10 12:04:12', 'Ac Gabriel Manalo', '09926591335', '2003-11-22', '', 'user');
 
 --
 -- Indexes for dumped tables
@@ -156,7 +163,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `insurance_claims`
@@ -174,7 +181,7 @@ ALTER TABLE `invoices`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
