febd5c2c1a2175d56f67b14ca9f3223f053aab69
acgbm <157551924+acgbm@users.noreply.github.com>
2024-12-10 13:11:52 +0800

    database update

--- clinicapi/clinic_billing.sql
+++ clinicapi/clinic_billing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2024 at 06:13 PM
+-- Generation Time: Dec 10, 2024 at 06:11 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `appointments` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL,
   `description` text DEFAULT NULL,
@@ -40,17 +41,10 @@ CREATE TABLE `appointments` (
 -- Dumping data for table `appointments`
 --
 
-INSERT INTO `appointments` (`id`, `user_id`, `date`, `time`, `description`, `created_at`) VALUES
-(1, 5, '2024-12-10', '10:00:00', 'General checkup', '2024-12-09 15:00:27'),
-(2, 5, '2024-12-10', '10:00:00', 'General checkup', '2024-12-09 15:10:17'),
-(4, 10, '2024-12-25', '10:00:00', NULL, '2024-12-09 15:54:02'),
-(5, 10, '2024-02-11', '08:00:00', NULL, '2024-12-09 15:58:50'),
-(6, 10, '2024-11-22', '07:45:00', NULL, '2024-12-09 16:12:37'),
-(7, 10, '2025-11-22', '19:00:00', NULL, '2024-12-09 16:16:01'),
-(8, 10, '2323-02-11', '12:32:00', NULL, '2024-12-09 16:29:47'),
-(9, 10, '2322-02-23', '02:32:00', NULL, '2024-12-09 16:33:45'),
-(10, 10, '0000-00-00', '02:32:00', 'adhd', '2024-12-09 16:37:24'),
-(11, 10, '2122-12-31', '14:32:00', 'I need eye checkup', '2024-12-09 16:37:51');
+INSERT INTO `appointments` (`id`, `user_id`, `name`, `date`, `time`, `description`, `created_at`) VALUES
+(12, 9, 'kevin', '2024-12-15', '10:30:00', 'Dental Checkup', '2024-12-10 04:43:40'),
+(13, 5, 'ac', '2024-03-04', '14:03:00', 'HAHAHA', '2024-12-10 04:52:10'),
+(14, 4, 'John Doe', '2024-02-23', '02:32:00', 'adhd', '2024-12-10 05:09:23');
 
 -- --------------------------------------------------------
 
@@ -122,11 +116,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `medical_records`, `created_a
 (3, 'acgabriel', '$2y$10$Da3TEt/jmHQ8fHN5yh1eQefRsv27QuffZLFV..PooYctNm0sNaWk2', NULL, '2024-12-08 11:07:24', 'Ac Gabriel', '', NULL, 'No known allergies. Previous surgery in 2015.'),
 (4, 'johndoe', '$2y$10$3bso9jc3i3Hw5ZgAZYwXgeYrmmlt0THcZoa.nr2rrWugeXpXA2nHq', NULL, '2024-12-08 12:08:31', 'John Doe', '1234567890', '1990-01-01', 'No known conditions'),
 (5, 'ac22', '$2y$10$.af5wH7IPoP5yiWrnmnrp.KNXmxVmmgnZKiVv7G60H2qUUijIXK4q', NULL, '2024-12-08 13:58:41', 'ac', '09926591335', '2003-11-22', ''),
-(6, 'ladyjohan', '$2y$10$Ln8DvG4AcusY6bpwRzUWg.fClKJF57eU4GmJo2W8UceGaNViHfMCS', NULL, '2024-12-08 14:05:48', 'Lady Johan Canapate', '09389077792', '2004-09-30', ''),
-(7, 'hex', '$2y$10$83BXTgejWJnxFOtlwrRe3OxJUxly6jdNLXOvrEvQxIE8KqeT6OPIi', NULL, '2024-12-08 14:30:14', 'HexsuDane', '09389077792', '2004-09-30', ''),
 (8, 'kevinhindipogi', '$2y$10$T/BtQV9apWwP2F4PH1lBheviREiOB/FT8wNzwkpm0/JuJMu59m/kW', NULL, '2024-12-09 03:09:28', 'kevin', '0912312312', '2003-02-23', ''),
-(9, 'johnkevin', '$2y$10$c5yrcd2TNbZ0fr9g0sFHTuwu3x2pmfPhHFUBeLqVU7h0unOgRpg5u', NULL, '2024-12-09 03:23:17', 'kevin', '1234567890', '1990-01-01', ''),
-(10, 'LJ', '$2y$10$zH0rBop6n/ALb46klQ2BQeYkRDoXRueCncbPNvS5COg5Z5D.xmp4O', NULL, '2024-12-09 09:27:14', 'Lady Johan Canapate', '09389077792', '2004-09-30', '');
+(9, 'johnkevin', '$2y$10$c5yrcd2TNbZ0fr9g0sFHTuwu3x2pmfPhHFUBeLqVU7h0unOgRpg5u', NULL, '2024-12-09 03:23:17', 'kevin', '1234567890', '1990-01-01', '');
 
 --
 -- Indexes for dumped tables
@@ -167,7 +158,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `insurance_claims`
