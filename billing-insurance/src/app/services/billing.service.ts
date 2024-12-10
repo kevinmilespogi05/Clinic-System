bd6edd3d1f78b1817f368c1bc9fdb547bad48023
kevinmilespogi05 <kevinmilesjulhusin05@gmail.com>
2024-12-10 22:03:46 +0800

    Update billing.service.ts

--- billing-insurance/src/app/services/billing.service.ts
+++ billing-insurance/src/app/services/billing.service.ts
@@ -16,22 +16,4 @@ export class BillingService {
     return this.http.post<any>(`${this.baseUrl}/api/auth/login.php`, { username, password });
   }
 
-  register(username: string, password: string, name: string, contact_number: string, date_of_birth: string): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/api/users/register.php`, {
-      username,
-      password,
-      name,
-      contact_number,
-      date_of_birth
-    });
-  }
-
-  // Error handler function for HTTP requests
-  private handleError(error: any): Observable<never> {
-    // Log the error to the console for debugging
-    console.error('An error occurred:', error);
-
-    // Return a user-friendly error message or rethrow the error
-    return throwError('Something went wrong while fetching appointments. Please try again later.');
-  }
 }
